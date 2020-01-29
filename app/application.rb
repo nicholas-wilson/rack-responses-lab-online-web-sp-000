c325e00e073b51a50431ed5521a55a178e8a48ce
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-29 22:20:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     current_hour = Time.now.hour
-    if current_hour <= 12
+    if current_hour < 12
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"
